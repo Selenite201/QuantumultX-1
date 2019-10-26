820961e1f2c165e6d8cdcfbea5b5f577bce04223
xixaxoqiqaqo <54254741+xixaxoqiqaqo@users.noreply.github.com>
2019-10-27 01:24:09 +0800

    Create adguard.js

---
+++ Filter/adguard.js
@@ -0,0 +1,15 @@
+var body = $response.body;
+var obj = JSON.parse(body);
+
+var arr = ["PREMIUM", "CN", "VALID", "DHAG8GKN3M"];
+
+obj['countryCode'] = arr[1];
+obj['expirationDate'] = 3043001166652;
+obj['licenseKey'] = arr[3];
+obj['licenseKeyStatus'] = arr[2];
+obj['status'] = arr[0];
+obj['subscription'] = null;
+
+body = JSON.stringify(obj);
+
+$done({body});
